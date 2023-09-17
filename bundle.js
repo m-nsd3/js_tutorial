0f9f99666b773e63f4fd2662c1dbb792660e2081
m-nsd3 <coutustom3@outlook.com>
2023-09-18 07:51:48 +0900

    9.2まで済

--- bundle.js
+++ bundle.js
@@ -1,15 +1,24 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 let Phrase = require("m-nsd3-palindrome");
 
-alert(new Phrase("Madam, I'm Adam.").palindrome());
-let string = prompt("パリンドロームをテストしたい文字列を入力してください:");
-let phrase = new Phrase(string);
+function palindromeTester() {
+  let string = prompt("パリンドロームをテストしたい文字列を入力してください:");
+  let phrase = new Phrase(string);
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}"はパリンドロームです`);
-} else {
-  alert(`"${phrase.content}"はパリンドロームではありません`)
+  if (phrase.palindrome()) {
+    alert(`"${phrase.content}"はパリンドロームです！`);
+  } else {
+    alert(`"${phrase.content}"はパリンドロームではありません`)
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    let form = document.querySelector("#palindromeTester");
+    form.addEventListener("submit", function() {
+      palindromeTester();
+    });
+  });
+
 },{"m-nsd3-palindrome":2}],2:[function(require,module,exports){
 module.exports = Phrase;
 
--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 let Phrase = require("m-nsd3-palindrome");
 
-alert(new Phrase("Madam, I'm Adam.").palindrome());
-let string = prompt("パリンドロームをテストしたい文字列を入力してください:");
-let phrase = new Phrase(string);
+function palindromeTester() {
+  let string = prompt("パリンドロームをテストしたい文字列を入力してください:");
+  let phrase = new Phrase(string);
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}"はパリンドロームです`);
-} else {
-  alert(`"${phrase.content}"はパリンドロームではありません`)
+  if (phrase.palindrome()) {
+    alert(`"${phrase.content}"はパリンドロームです！`);
+  } else {
+    alert(`"${phrase.content}"はパリンドロームではありません`)
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    let form = document.querySelector("#palindromeTester");
+    form.addEventListener("submit", function() {
+      palindromeTester();
+    });
+  });
